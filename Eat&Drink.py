0a216294ce1a115cc64f81d448cc2844c9589176
Dziugas <dziugas.tornau@gmail.com>
2018-07-23 12:01:15 +0300

    MedicalTourism & EatDrink work well

--- Eat&Drink.py
+++ Eat&Drink.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
 
 import csv
 import time
@@ -11,15 +11,15 @@ import time
 #fix the starting time of running the script
 start_time = time.clock()
 
-# Open a new csv file to save(write) the results
-outputFile = open('eatDrink.csv', 'w', newline='', encoding='utf-8')
+# Open a new csv file to save(write) the results to
+outputFile = open('eat-drink.csv', 'w', newline='', encoding='utf-8')
 outputWriter = csv.writer(outputFile)
 
-# Create a list with column titles and write it to csv as the first line
-column_names = ['ID', 'Source', 'Title', 'Address', 'Phone', 'Email', 'Website', 'Working Hours', 'Category']
+# Create a list with column titles and write it to the csv as the first line
+column_names = ['ID', 'Source', 'Title', 'Address', 'Phone', 'Email', 'Website', 'Working Hours', 'Description', 'Category']
 outputWriter.writerow(column_names)
 
-# empty list for adding new line of data to write to csv
+# empty list for adding a new line of data to write to csv
 empty_list = []
 
 # xpaths for the wanted elements
@@ -29,16 +29,11 @@ phone_xpath = '//*[@id="body"]/div[2]/div[1]/div[3]/div/span[2]/a'
 email_xpath ='//*[@id="body"]/div[2]/div[1]/div[3]/div/span[3]/a'
 website_xpath = '//*[@id="body"]/div[2]/div[1]/div[3]/div/span[4]/a'
 working_hours_xpath = '//*[@id="body"]/div[2]/div[1]/div[3]/div/span[5]'
+description_xpath = '//*[@id="body"]/div[2]/div[2]/div'
 
 #list with all the xpaths
-xpaths = [title_xpath, address_xpath, phone_xpath, email_xpath, website_xpath, working_hours_xpath]
+xpaths = [title_xpath, address_xpath, phone_xpath, email_xpath, website_xpath, working_hours_xpath, description_xpath]
 
-# a function that finds all the categories(tags) and extract links from them
-def categories():
-    categories = driver.find_elements_by_css_selector('#body > div.inside-nav > div > nav > a')
-    # del cat_links[0]
-    del categories[0]
-    return categories
 
 # a function that keeps clicking the 'More' button until all results are displayed
 def extend_page():
@@ -50,67 +45,60 @@ def extend_page():
         except NoSuchElementException:
             break
 
-# a function that finds all the elements (titles) and extract links from them
-def links():
+# a function that finds all the elements (links to them) and their respective categories
+def links_and_categories():
     titles = driver.find_elements_by_class_name('product-title')
     links = [title.get_attribute('href') for title in titles]
-    return links
+    categories = driver.find_elements_by_class_name('category-title')
+    category_names = [category.text for category in categories]
+    dictionary = dict(zip(links, category_names))
+    # print(dictionary)
+    return dictionary
 
-# variable to generate IDs
+# a variable to generate IDs
 id = 1
 
-# Create a driver and open a link in Chrome
+# Create a driver, open the link in Chrome and escape the popup
 driver = webdriver.Chrome()
 driver.get('http://visit.kaunas.lt/en/eat-and-drink/')
-
-# escape the popup
 ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
-cats = categories()
+# run the function that extends the page and
+# give 3 secs for the browser to load all elements
+extend_page()
+time.sleep(3)
 
-for category in cats:
+# get a variable for the dictionary of links and categories
+dictionary = links_and_categories()
 
-    category_link = category.get_attribute('href')
-    category_text = category.text
-    #run the function that extends the page
-    driver.get(category_link)
-    extend_page()
+#loop the links and categories in the dictionary
+for link, category in dictionary.items():
+    #add the id and the source page to the empty list
+    empty_list.append(id)
+    empty_list.append(link)
 
-    #give 3 secs for the browser to load all elements
+    # go to each link
+    # find elements and append to the empty list, or enter empty values if elements not found
+    driver.get(link)
     time.sleep(3)
 
-    #run the function that collects the links and assign the result to the list variable 'titles'
-    title_links = links()
+    for object in xpaths:
+        try:
+            element = driver.find_element_by_xpath(object).text
+            empty_list.append(element)
+        except NoSuchElementException:
+            empty_list.append('')
 
-    #print how many results we got
-    print(len(links()))
+    #add the category to the list from the dictionary
+    empty_list.append(category)
 
-        # Go through the pages and select the wanted elements
-    for link in title_links:
-        #add the source page to the table
-        empty_list.append(id)
-        empty_list.append(link)
+    # write the row with appended element values (empty_list) to the output .csv file
+    outputWriter.writerow(empty_list)
 
-        #open link in the browser
-        driver.get(link)
-
-        # find elements and append to the empty list b, or enter empty values if elements not found
-        for object in xpaths:
-            try:
-                element = driver.find_element_by_xpath(object).text
-                empty_list.append(element)
-            except NoSuchElementException:
-                empty_list.append('')
-
-        empty_list.append(category_text)
-        # write the element to the output .csv file
-        outputWriter.writerow(empty_list)
-
-        # empty the list for the next line of data from the next page
-        empty_list = []
-
-        # increase the id by one
-        id+=1
+    # empty the list for the next line of data from the next page
+    empty_list = []
+    # increase the id by one
+    id+=1
 
 # close the .csv file
 outputFile.close()
